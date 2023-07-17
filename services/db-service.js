1e1330381cce03afe8fff727be1cca86c6997cf2
labstraction <45163263+labstraction@users.noreply.github.com>
2023-07-17 13:30:39 +0200

    Update db-service.js

--- services/db-service.js
+++ services/db-service.js
@@ -1,5 +1,6 @@
 class DBService{
 
+    //GET
     static getAllTodos(){
         const url = 'https://64b512c1f3dbab5a95c6a48c.mockapi.io/todos';
         return fetch(url)
@@ -8,11 +9,22 @@ class DBService{
         .catch(error => console.log(error.message));
     }
 
+    //DELETE
     static deleteTodo(id){
         console.log('delete', id);
         const deleteUrl = 'https://64b512c1f3dbab5a95c6a48c.mockapi.io/todos/' + id;
         console.log(deleteUrl);
         return fetch(deleteUrl, {method: 'delete'}).then(resp => resp.json());
+    }
+
+    //PUT
+    static updateTodo(todo){
+
+    }
+
+    //UPDATE
+    static saveTodo(todo){
+
     }
 
 
